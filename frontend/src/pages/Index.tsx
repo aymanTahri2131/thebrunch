92d91865d32141963d285fca3750bb462d2ebcd8
aymanTahri2131 <th.ayman.96@gmail.com>
2025-11-18 20:29:02 +0100

    Adjust layout and styling of the main content area 13243431

--- frontend/src/pages/Index.tsx
+++ frontend/src/pages/Index.tsx
@@ -14,7 +14,7 @@ const Index = () => {
       <Hero />
       <About />
       <ReveillonMenu />
-      <div className="flex w-full p-12 gap-4 h-[280px] bg-accent/50 text-white">
+      <div className="flex w-full items-center justify-center p-12 gap-12 h-[350px] bg-accent/50 text-white">
         <div>
           <Leaf sise={20} />
           <h2 className="text-[#4a3b36]">MATIÈRES PREMIÈRES SÉLECTIONNÉES</h2>
