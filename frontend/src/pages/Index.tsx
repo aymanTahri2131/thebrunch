19e920ebd3261e42fbcdde7c9a79c676fea8d69f
aymanTahri2131 <th.ayman.96@gmail.com>
2025-11-18 20:54:29 +0100

    Fix prop name for Leaf, Heart, and Star components dcececwws

--- frontend/src/pages/Index.tsx
+++ frontend/src/pages/Index.tsx
@@ -16,18 +16,18 @@ const Index = () => {
       <ReveillonMenu />
       <div className="flex flex-col sm:flex-col lg:flex-row w-full items-center justify-center p-12 gap-12 h-auto sm:h-auto lg:h-[350px] bg-accent/50 text-white">
         <div className="flex flex-col items-center justify-center gap-6 text-center">
-          <Leaf style={height:"44px", width:"44px"} />
+          <Leaf size={40} />
           <h2 className="text-[#4a3b36]">MATIÈRES PREMIÈRES SÉLECTIONNÉES</h2>
           <p className="text-center max-w-[400px]">Nos matières premières sont soigneusement sélectionnées afin de vous offrir la meilleure qualité pour vos plus beaux jours.</p>
         </div>
         <div className="flex flex-col items-center justify-center gap-6 text-center">
-          <Heart sise={40} />
+          <Heart size={40} />
           <h2 className="text-[#4a3b36]">SAVOIR-FAIRE ARTISANAL</h2>
           <p className="text-center max-w-[400px]">Un travail artisanal fait avec amour et savoir-faire, pour transformer vos moments les plus importants en doux souvenirs gourmands.</p>
         
         </div>
         <div className="flex flex-col items-center justify-center gap-6 text-center">
-          <Star sise={40} />
+          <Star size={40} />
           <h2 className="text-[#4a3b36]">SATISFACTION CLIENT</h2>
           <p className="text-center max-w-[400px]">Nous plaçons vos besoins et vos retours au cœur de notre démarche, pour faire de chaque commande une expérience unique et réussie.</p>
         
