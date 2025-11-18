31acd4a0650d35cd00235750b08f6e371403882c
aymanTahri2131 <th.ayman.96@gmail.com>
2025-11-18 21:14:33 +0100

    Add text size class to footer descriptionkjjj

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -10,7 +10,7 @@ export const Footer = () => {
           {/* Brand */}
           <div>
             <img src={Logo} className="w-20 mb-4" alt="Traiteur Oriental & Brunch à Strasbourg" />
-            <p className="text-muted-foreground">
+            <p className="text-muted-foreground text-xs">
               Traiteur basé à Strasbourg et spécialisé dans la création d'événements sur mesure.
               Nous mettons un point d'honneur à proposer des créations artisanales,
               élégantes et adaptées à toutes vos occasions.
