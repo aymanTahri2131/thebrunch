ecc039f646a4f8f9af6936cc752ec13d79dfbe4b
aymanTahri2131 <th.ayman.96@gmail.com>
2025-11-18 20:41:40 +0100

    Fix layout and update text styles in Index page hgwusgwus

--- frontend/src/pages/Index.tsx
+++ frontend/src/pages/Index.tsx
@@ -15,21 +15,21 @@ const Index = () => {
       <About />
       <ReveillonMenu />
       <div className="flex w-full items-center justify-center p-12 gap-12 h-[350px] bg-accent/50 text-white">
-        <div className="flex flex-col items-center justyfy-center gap-6 ">
-          <Leaf sise={20} />
+        <div className="flex flex-col items-center justify-center gap-6 text-center">
+          <Leaf sise={32} />
           <h2 className="text-[#4a3b36]">MATIÈRES PREMIÈRES SÉLECTIONNÉES</h2>
-          <p>Nos matières premières sont soigneusement sélectionnées afin de vous offrir la meilleure qualité pour vos plus beaux jours.</p>
+          <p className="text-center max-w-[450px]">Nos matières premières sont soigneusement sélectionnées afin de vous offrir la meilleure qualité pour vos plus beaux jours.</p>
         </div>
-        <div>
-          <Heart sise={20} />
+        <div className="flex flex-col items-center justify-center gap-6 text-center">
+          <Heart sise={32} />
           <h2 className="text-[#4a3b36]">SAVOIR-FAIRE ARTISANAL</h2>
-          <p>Un travail artisanal fait avec amour et savoir-faire, pour transformer vos moments les plus importants en doux souvenirs gourmands.</p>
+          <p className="text-center max-w-[450px]">Un travail artisanal fait avec amour et savoir-faire, pour transformer vos moments les plus importants en doux souvenirs gourmands.</p>
         
         </div>
-        <div>
-          <Star sise={20} />
+        <div className="flex flex-col items-center justify-center gap-6 text-center">
+          <Star sise={32} />
           <h2 className="text-[#4a3b36]">SATISFACTION CLIENT</h2>
-          <p>Nous plaçons vos besoins et vos retours au cœur de notre démarche, pour faire de chaque commande une expérience unique et réussie.</p>
+          <p className="text-center max-w-[450px]">Nous plaçons vos besoins et vos retours au cœur de notre démarche, pour faire de chaque commande une expérience unique et réussie.</p>
         
         </div>
       </div>
