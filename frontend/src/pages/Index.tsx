8eabc167be6666c10cd9ca456f30384c49eb12ce
aymanTahri2131 <th.ayman.96@gmail.com>
2025-11-18 20:47:52 +0100

    Change icon sizes and container height in Index.tsx mjgg
    Updated the size of icons in the Index page and adjusted the height of the container.

--- frontend/src/pages/Index.tsx
+++ frontend/src/pages/Index.tsx
@@ -14,20 +14,20 @@ const Index = () => {
       <Hero />
       <About />
       <ReveillonMenu />
-      <div className="flex flex-col sm:flex-col lg:flex-row w-full items-center justify-center p-12 gap-12 h-[350px] bg-accent/50 text-white">
+      <div className="flex flex-col sm:flex-col lg:flex-row w-full items-center justify-center p-12 gap-12 h-auto sm:h-auto lg:h-[350px] bg-accent/50 text-white">
         <div className="flex flex-col items-center justify-center gap-6 text-center">
-          <Leaf sise={32} />
+          <Leaf sise={40} />
           <h2 className="text-[#4a3b36]">MATIÈRES PREMIÈRES SÉLECTIONNÉES</h2>
           <p className="text-center max-w-[400px]">Nos matières premières sont soigneusement sélectionnées afin de vous offrir la meilleure qualité pour vos plus beaux jours.</p>
         </div>
         <div className="flex flex-col items-center justify-center gap-6 text-center">
-          <Heart sise={32} />
+          <Heart sise={40} />
           <h2 className="text-[#4a3b36]">SAVOIR-FAIRE ARTISANAL</h2>
           <p className="text-center max-w-[400px]">Un travail artisanal fait avec amour et savoir-faire, pour transformer vos moments les plus importants en doux souvenirs gourmands.</p>
         
         </div>
         <div className="flex flex-col items-center justify-center gap-6 text-center">
-          <Star sise={32} />
+          <Star sise={40} />
           <h2 className="text-[#4a3b36]">SATISFACTION CLIENT</h2>
           <p className="text-center max-w-[400px]">Nous plaçons vos besoins et vos retours au cœur de notre démarche, pour faire de chaque commande une expérience unique et réussie.</p>
         
