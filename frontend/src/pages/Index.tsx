dfc6ca85ce7129755c1a0aee6a758ea098cf058a
elhocinkania <elhocinkaniafb@gmail.com>
2025-11-22 19:19:43 +0100

    Jjfjdre

--- frontend/src/pages/Index.tsx
+++ frontend/src/pages/Index.tsx
@@ -3,14 +3,13 @@ import { Hero } from "@/components/Hero";
 import { About } from "@/components/About";
 import { ReveillonMenu } from "@/components/ReveillonMenu";
 import { Realisations } from "@/components/Realisations";
-import { Contact } from "@/components/Contact";
+
 import { Footer } from "@/components/Footer";
 import FAQ from "@/components/FAQ";
 import { RET } from "@/components/RET";
 import { AVTG } from "@/components/AVTG";
 import { Leaf, Heart, Star, Quote } from "lucide-react";
-
-const Index = () => {
+niconst Index = () => {
   return (
     <div className="min-h-screen">
       <Navigation />
@@ -21,7 +20,6 @@ const Index = () => {
       <RET />
       <Realisations />
       <FAQ />
-      <Contact />
       <Footer />
     </div>
   );
