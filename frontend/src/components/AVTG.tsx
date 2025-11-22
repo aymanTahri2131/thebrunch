63af44c8aac4501867449fdc92ffbb78ab40e9e4
elhocinkania <elhocinkaniafb@gmail.com>
2025-11-22 03:06:04 +0100

    Create AVTG.tsx

---
+++ frontend/src/components/AVTG.tsx
@@ -0,0 +1,45 @@
+// components/AVTG.tsx
+import { Leaf, Heart, Star } from "lucide-react";
+
+export const AVTG = () => {
+  return (
+    <section>
+      <div className="flex flex-col sm:flex-col lg:flex-row w-full items-center justify-center p-12 gap-12 h-auto sm:h-auto lg:h-[350px] bg-[#99771b]/40 text-white">
+        
+        <div className="flex flex-col items-center justify-center gap-6 text-center">
+          <Leaf size={40} />
+          <h2 className="text-[#4a3b36] font-semibold text-lg" style={{ fontFamily: "Poppins, sans-serif" }}>
+            MATIÈRES PREMIÈRES SÉLECTIONNÉES
+          </h2>
+          <p className="text-center max-w-[400px] text-lg">
+            Des ingrédients soigneusement choisis.
+            Nous choisissons avec le plus grand soin des ingrédients d’exception afin de vous garantir une qualité irréprochable pour sublimer chacun de vos moments précieux.
+          </p>
+        </div>
+
+        <div className="flex flex-col items-center justify-center gap-6 text-center">
+          <Heart size={40} />
+          <h2 className="text-[#4a3b36] font-semibold text-lg" style={{ fontFamily: "Poppins, sans-serif" }}>
+            SAVOIR-FAIRE ARTISANAL
+          </h2>
+          <p className="text-center max-w-[400px] text-lg">
+            Un artisanat fait avec passion.
+            Un travail artisanal réalisé avec passion et maîtrise, pour transformer vos instants les plus marquants en souvenirs gourmands raffinés et mémorables.
+          </p>
+        </div>
+
+        <div className="flex flex-col items-center justify-center gap-6 text-center">
+          <Star size={40} />
+          <h2 className="text-[#4a3b36] font-semibold text-lg" style={{ fontFamily: "Poppins, sans-serif" }}>
+            SATISFACTION CLIENT
+          </h2>
+          <p className="text-center max-w-[400px] text-lg">
+            Votre satisfaction au cœur de notre démarche.
+            Nous plaçons vos attentes au centre de notre démarche afin de faire de chaque commande une expérience sur-mesure, unique et parfaitement réussie.
+          </p>
+        </div>
+
+      </div>
+    </section>
+  );
+};
