2d86b02a736e1ca73608c91e42f5feaed88d2fd0
elhocinkania <elhocinkaniafb@gmail.com>
2025-11-23 03:19:29 +0100

    Update Navigation.tsxmmmm

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -134,7 +134,7 @@ export const Navigation = () => {
     { name: "Brunch", path: "/brunch" },
     { name: "Menu Réveillon", path: "/menu-reveillon", scrollTo: "reveillon-menu" },
     { name: "Contact", path: "/contact", isContact: true },
-    { name: "FAQ", path: "/FAQ" },
+    { name: "FAQ", path: "/questions" },
   ];
 
   return (
