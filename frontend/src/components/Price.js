4525f65d66c269f23d722c47bc51040faabb4667
elhocinkania <elhocinkaniafb@gmail.com>
2025-11-23 05:00:45 +0100

    Delete frontend/src/components/Price.js

--- frontend/src/components/Price.js
+++
@@ -1,9 +0,0 @@
-// components/Price.js
-export default function Price({ amount }) {
-  const formattedPrice = new Intl.NumberFormat('fr-FR', {
-    style: 'currency',
-    currency: 'EUR',
-  }).format(amount);
-
-  return <span>{formattedPrice}</span>;
-}
