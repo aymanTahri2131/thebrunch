5d3bd1babe4f900e32db3fb04c019a238624d567
elhocinkania <elhocinkaniafb@gmail.com>
2025-11-26 23:53:03 +0100

    Update Lunch.tsxudy77

--- frontend/src/pages/Lunch.tsx
+++ frontend/src/pages/Lunch.tsx
@@ -111,8 +111,9 @@ const Lunch = () => {
               Menu Lunch
             </h1>
           </div>
-            <p className="text-sm md:text-base mb-8 text-white/90 max-w-2xl mx-auto" style={{ fontFamily: '"Inconsolata", monospace' }}>
-             Découvrez une sélection raffinée, soigneusement imaginée pour vous offrir une expérience culinaire généreuse, inventive et subtilement orchestrée autour des saveurs les plus délicates.</p>
+        <p className="text-sm md:text-base mb-8 text-white/90 max-w-2xl mx-auto" style={{ fontFamily: '"Inconsolata", monospace' }}>
+          Découvrez une sélection raffinée, soigneusement imaginée pour vous offrir une expérience culinaire généreuse, inventive et subtilement orchestrée autour des saveurs les plus délicates.
+        </p>    
         </div>
       </section>
 
