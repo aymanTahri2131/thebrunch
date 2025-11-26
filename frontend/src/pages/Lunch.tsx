c3a1ede84e8ad82732ee39f011b72557dee9c5f1
elhocinkania <elhocinkaniafb@gmail.com>
2025-11-26 23:46:38 +0100

    Update Lunch.tsx76y

--- frontend/src/pages/Lunch.tsx
+++ frontend/src/pages/Lunch.tsx
@@ -7,7 +7,7 @@ import { Badge } from "@/components/ui/badge";
 import { Link } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import { Star, ChefHat, Utensils, Package, Wine, Loader2 } from "lucide-react";
-
+import "@fontsource/inconsolata"; 
 const Lunch = () => {
   const [lunchData, setLunchData] = useState(null);
   const [loading, setLoading] = useState(true);
