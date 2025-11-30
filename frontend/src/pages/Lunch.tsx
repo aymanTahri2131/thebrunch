36d9590b06dcf1161e1cab6e174dbe175f6e63a1
elhocinkania <elhocinkaniafb@gmail.com>
2025-11-30 23:54:14 +0100

    Update Lunch.tsxsddu

--- frontend/src/pages/Lunch.tsx
+++ frontend/src/pages/Lunch.tsx
@@ -111,10 +111,6 @@ const Lunch = () => {
               Menu Lunch
             </h1>
           </div>
-        <p 
-  {/* Content */}
-      <div className="relative z-10 container mx-auto px-4 text-center text-white">
-        <img src={HeroImage} alt="Traiteur Oriental basé à Strasbourg animate-fade-in" className="w-80 mx-auto mb-8" /> 
         <p className="text-sm md:text-base mb-8 text-white/90 max-w-2xl mx-auto" style={{ fontFamily: '"Inconsolata", monospace' }}>
     Découvrez une sélection raffinée, soigneusement imaginée pour vous offrir une expérience culinaire généreuse, inventive et subtilement orchestrée autour des saveurs les plus délicates
         </p>
