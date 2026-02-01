fd4148114ab7b8ec2353bc360c42e9a6a6d5181f
elhocinkania <elhocinkaniafb@gmail.com>
2026-02-02 00:14:03 +0100

    Add 'Menu Ramadan' link to navigationiskxjjs

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -132,6 +132,7 @@ export const Navigation = () => {
     { name: "Accueil", path: "/", isHome: true },
     { name: "Lunch", path: "/lunch" },
     { name: "Brunch", path: "/brunch" },
+    { name: "Menu Ramadan", path: "/menu-reveillon", scrollto: "reveillon-menu" },
     { name: "Contact", path: "/contact"},
   ];
 
